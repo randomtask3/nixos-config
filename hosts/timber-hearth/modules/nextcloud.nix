618f1af845cf7d8b93f290319f427b2fd825ee7c
randomtask3 <nbrooker@proton.me>
2024-03-20 18:21:26 -0400

    Trying to add Nextcloud/Samba to timber-hearth

--- hosts/timber-hearth/modules/nextcloud.nix
+++ hosts/timber-hearth/modules/nextcloud.nix
@@ -1,21 +1,64 @@
-{ pkgs,
-  ...
-}:{
-  environment.etc."nextcloud-admin-pass".text = "PWD";
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud28;
-    hostName = "localhost";
-    config.adminpassFile = "/etc/nextcloud-admin-pass";
+{ self, config, lib, pkgs, ... }:
 
-    extraApps = {
-      inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
-      memories = pkgs.fetchNextcloudApp {
-          sha256 = "sha256-Xr1SRSmXo2r8yOGuoMyoXhD0oPVm/0/ISHlmNZpJYsg=";
-          url = "https://github.com/pulsejet/memories/releases/download/v6.2.2/memories.tar.gz";
-          license = "agpl3";
+{
+  services = {
+    nginx.virtualHosts = {
+      "cloud.example.com" = {
+        forceSSL = true;
+        enableACME = true;
+      };
+
+      #"onlyoffice.example.com" = {
+      #  forceSSL = true;
+      #  enableACME = true;
+      #};
+    };
+
+    nextcloud = {
+      enable = true;
+      hostName = "cloud.example.com";
+
+       # Need to manually increment with every major upgrade.
+      package = pkgs.nextcloud27;
+
+      # Let NixOS install and configure the database automatically.
+      database.createLocally = true;
+
+      # Let NixOS install and configure Redis caching automatically.
+      configureRedis = true;
+
+      # Increase the maximum file upload size to avoid problems uploading videos.
+      maxUploadSize = "16G";
+      https = true;
+      enableBrokenCiphersForSSE = false;
+
+      autoUpdateApps.enable = true;
+      extraAppsEnable = true;
+      extraApps = with config.services.nextcloud.package.packages.apps; {
+        # List of apps we want to install and are already packaged in
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+        inherit calendar contacts mail notes onlyoffice tasks;
+
+        # Custom app installation example.
+        cookbook = pkgs.fetchNextcloudApp rec {
+          url =
+            "https://github.com/nextcloud/cookbook/releases/download/v0.10.2/Cookbook-0.10.2.tar.gz";
+          sha256 = "sha256-XgBwUr26qW6wvqhrnhhhhcN4wkI+eXDHnNSm1HDbP6M=";
+        };
+      };
+
+      config = {
+        overwriteProtocol = "https";
+        defaultPhoneRegion = "PT";
+        dbtype = "pgsql";
+        adminuser = "admin";
+        adminpassFile = "/path/to/nextcloud-admin-pass";
       };
     };
-    extraAppsEnable = true;
+
+    #onlyoffice = {
+    #  enable = true;
+    #  hostname = "onlyoffice.example.com";
+    #};
   };
 }
