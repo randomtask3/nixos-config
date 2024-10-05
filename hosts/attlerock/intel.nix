03ab8af822fc28cdb5ef6b74288bd10d702bbb05
randomtask3 <nbrooker@proton.me>
2024-10-05 09:56:17 -0400

    Changed intel configuration

--- hosts/attlerock/intel.nix
+++ hosts/attlerock/intel.nix
@@ -8,9 +8,8 @@
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      vpl-gpu-rt          # for newer GPUs on NixOS >24.05 or unstable
-      # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
-      # intel-media-sdk   # for older GPUs
+      onevpl-intel-gpu    # NixOS <=24.05
+      #vpl-gpu-rt         # NixOS >24.05 or unstable
     ];
   };
 }
