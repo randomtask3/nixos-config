3b06ea9dbffeade29482504c83281c68c4cf8275
randomtask3 <nbrooker@proton.me>
2024-10-05 09:51:11 -0400

    Changed intel configuration

--- hosts/attlerock/intel.nix
+++ hosts/attlerock/intel.nix
@@ -1,11 +1,12 @@
-{ config,
+{ pkgs,
+  config,
   lib,
   ...
 }:{
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      ... # your Open GL, Vulkan and VAAPI drivers
       vpl-gpu-rt          # for newer GPUs on NixOS >24.05 or unstable
       # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
       # intel-media-sdk   # for older GPUs
