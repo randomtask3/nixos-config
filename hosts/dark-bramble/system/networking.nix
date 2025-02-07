d05f2f189f5a47f01e56171bb632d5a2632bcbbd
randomtask3 <nbrooker@proton.me>
2025-02-07 15:41:07 -0500

    Re-enabled dhcp on attlerock and dark-bramble

--- hosts/dark-bramble/system/networking.nix
+++ hosts/dark-bramble/system/networking.nix
@@ -2,17 +2,17 @@
   networking = {
     networkmanager.enable = true;
     
-    dhcpcd.enable = false;
-    defaultGateway = "10.66.0.1";
-    nameservers = [ "1.1.1.1" "8.8.8.8" ];
-    interfaces.enp34s0 = {
-      wakeOnLan.enable = true;
-      #wakeOnLan.policy = "magic";
-      ipv4.addresses = [{
-        address = "10.66.0.11";
-        prefixLength = 24;
-      }];
-    };
+    #dhcpcd.enable = false;
+    #defaultGateway = "10.66.0.1";
+    #nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    #interfaces.enp34s0 = {
+    #  wakeOnLan.enable = true;
+    #  #wakeOnLan.policy = "magic";
+    #  ipv4.addresses = [{
+    #    address = "10.66.0.11";
+    #    prefixLength = 24;
+    #  }];
+    #};
 
     hosts = {
       "127.0.0.2" = ["other-localhost"];
