5178d69651321305544ca1d9734b18316adf65b8
randomtask3 <nbrooker@proton.me>
2025-02-08 20:14:33 -0500

    Changed up folder structure

--- hosts/dark-bramble/default.nix
+++ hosts/dark-bramble/default.nix
@@ -3,7 +3,6 @@
   ... 
 }:{
   imports = [
-    ./desktop
     ./packages
     ./system
     ../../modules
--- hosts/dark-bramble/system/default.nix
+++ hosts/dark-bramble/system/default.nix
@@ -5,6 +5,7 @@
 }:{
   imports = [
     ./bootloader.nix
+    ./desktop.nix
     ./hardware-configuration.nix
     ./networking.nix
     #./thinkpad-p52s.nix
