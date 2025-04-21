2620cad8c24623c02b339f22153370f180ef9a44
randomtask3 <nbrooker@proton.me>
2025-04-21 15:44:08 -0400

    upgraded and added GUIX to grub

--- hosts/timber-hearth/system/bootloader.nix
+++ hosts/timber-hearth/system/bootloader.nix
@@ -9,13 +9,13 @@
       device = "nodev";
       useOSProber = true;
       extraEntries = ''
-        #menuentry "Windows 10" --class guix --class os {
-        #  insmod part_gpt
-        #  insmod ext2
-        #  insmod fat
-        #  set root=(hd3,0)
-        #  chainloader /EFI/Boot/bootx64.efi
-        #}
+        menuentry "GNU GUIX" --class guix --class os {
+          insmod part_gpt
+          insmod ext2
+          insmod fat
+          set root=(hd3,0)
+          chainloader /EFI/Boot/bootx64.efi
+        }
         #menuentry "openSUSE" --class opensuse --class os {
         #  insmod part_gpt
         #  insmod ext2
