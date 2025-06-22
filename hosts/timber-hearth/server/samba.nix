59b3a1f786ee3bee5f43deb2daf816d93b3dbadf
randomtask3 <nbrooker@proton.me>
2025-06-22 13:29:10 -0400

    playing with samba shares

--- hosts/timber-hearth/server/samba.nix
+++ hosts/timber-hearth/server/samba.nix
@@ -15,17 +15,9 @@
         "read only" = "no";
         "guest ok" = "no";
         "valid_user" = "nick";
-      };
-
-      "PS2SMB" = {
-        "path" = "/home/nick/Media/disk2/Games/Emulators/PS2";
-        "browseable" = "yes";
-        "read only" = "no";
-        "guest ok" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "username";
-        "force group" = "groupname";
+        #"force user" = "nobody";
+        #"available" = "yes";
+        #"public" = "yes";
       };
     };
   };
