ce95752e14c53c551d050c7e7be53f32cf933049
randomtask3 <nbrooker@proton.me>
2026-08-02 11:43:27 -0400

    adding opengl dev env

---
+++ develop/opengl_cpp/flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "OpenGL C++ development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Toolchain
+            clang
+            clang-tools
+            lldb
+
+            cmake
+            ninja
+            pkg-config
+            gnumake
+
+            # OpenGL
+            mesa
+            libGL
+            libGLU
+
+            # Windowing
+            glfw
+
+            # Extensions
+            glew
+
+            # Math
+            glm
+
+            sdl2        # audio/input if desired
+            openal
+
+            # Debugging
+            renderdoc
+
+            # Misc
+            git
+            gdb
+
+            # vscode integration
+            clang-tools
+            bear
+            cmake-language-server
+          ];
+
+          shellHook = ''
+            export CC=clang
+            export CXX=clang++
+
+            echo "OpenGL development shell"
+            echo "Compiler: $(clang++ --version | head -n1)"
+          '';
+        };
+      });
+}
--- hosts/brittle-hollow/system/filesystem.nix
+++ hosts/brittle-hollow/system/filesystem.nix
@@ -13,4 +13,14 @@
       in ["${automount_opts},credentials=/sec/smb-secrets,uid=1000"];
       #,uid=1000,rw,users
   };
+  fileSystems."/home/nick/Shares/Interloper" = {
+    device = "//interloper/Interloper-Home";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/sec/smb-secrets,uid=1000"];
+      #,uid=1000,rw,users
+  };
 }
