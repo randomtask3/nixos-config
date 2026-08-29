71c7287f74d64a9edd7d497275dd0ee28b43b498
randomtask3 <nbrooker@proton.me>
2026-08-28 20:17:35 -0400

    added chicago95 theme

--- theme/theme.nix
+++ theme/theme.nix
@@ -67,17 +67,69 @@ let
     '';
   };
 
+  chicago95 = pkgs.stdenvNoCC.mkDerivation {
+    pname = "chicago95";
+    version = "git";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "grassmunk";
+      repo = "Chicago95";
+      rev = "master";
+      hash = "sha256-Qtk9cmWlppZA527VFFi64IuV4eDSkYh4YOeK5/TPmkw=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/themes
+      mkdir -p $out/share/icons
+
+      cp -r Theme/Chicago95 $out/share/themes/
+      cp -r Icons/* $out/share/icons/
+    '';
+  };
+
+  pixelitos-pixel-icons = pkgs.stdenvNoCC.mkDerivation {
+    pname = "pixelitos-icon-theme";
+    version = "20260721";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "ItsZariep";
+      repo = "pixelitos-icon-theme";
+      rev = "20260721";
+      hash = "sha256-UXNyY5LdaiKtcNQ/E5gmGt++MNZACW1eO6jRvMJ5YV0=";
+    };
+
+    nativeBuildInputs = [
+      pkgs.imagemagick
+      pkgs.gtk3
+    ];
+
+    installPhase = ''
+      cd pixelitos-dark
+
+      # Generate the 128x128 icons and icon cache.
+      bash ./compile-icons.sh
+
+      # Install the completed theme.
+      mkdir -p $out/share/icons
+      cp -r . $out/share/icons/pixelitos-dark
+
+      # The build script isn't needed after installation.
+      rm -f $out/share/icons/pixelitos-dark/compile-icons.sh
+    '';
+  };
+
 in
 {
   environment.systemPackages = [
     beosIcons
+    pixelitos-pixel-icons
     buuf-nestort
     nashville96
     pkgs.papirus-icon-theme
     pkgs.mate-icon-theme
     pkgs.arc-icon-theme
     pkgs.numix-icon-theme
-    pkgs.chicago95
+    chicago95
     pkgs.catppuccin-gtk
     pkgs.gruvbox-dark-gtk
     #pkgs.gruvbox-gtk-theme
