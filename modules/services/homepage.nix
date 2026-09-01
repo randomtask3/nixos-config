8374885e5c897399c9cb5f3d7387f8b2b8f144f9
randomtask3 <nbrooker@proton.me>
2026-08-31 20:26:50 -0400

    setting up homepage

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -1,107 +1,111 @@
-services.homepage-dashboard = {
-  enable = true;
+{ pkgs,
+  ...
+}:{
+  services.homepage-dashboard = {
+    enable = true;
+    package = pkgs.homepage-dashboard;
+    listenPort = 8082;
+    openFirewall = true;
 
-  listenPort = 3004;
-  openFirewall = true;
+    allowedHosts = "10.66.0.10:8082";
 
-  allowedHosts = "192.168.1.100:3000";
+    settings = {
+      title = "Home Server";
+      theme = "dark";
+      color = "slate";
 
-  settings = {
-    title = "Home Server";
-    theme = "dark";
-    color = "slate";
+      layout = {
+        "Network" = {
+          style = "columns";
+          columns = 3;
+        };
 
-    layout = {
-      "Network" = {
-        style = "columns";
-        columns = 3;
-      };
-
-      "Services" = {
-        style = "columns";
-        columns = 3;
+        "Services" = {
+          style = "columns";
+          columns = 3;
+        };
       };
     };
+
+    bookmarks = [
+      {
+        "Network" = [
+          {
+            "TP-Link AX3000" = [
+              {
+                abbr = "TP";
+                href = "http://10.66.0.1";
+              }
+            ];
+          }
+        ];
+      }
+    ];
+
+    services = [
+      {
+        "Network" = [
+          {
+            "AdGuard Home" = {
+              href = "http://10.66.0.10:3002";
+              description = "DNS & Ad Blocking";
+              icon = "adguard-home.png";
+            };
+          }
+
+          {
+            "Router" = {
+              href = "http://10.66.0.1";
+              description = "TP-Link AX3000";
+              icon = "router.png";
+            };
+          }
+        ];
+      }
+
+      {
+        "Services" = [
+          {
+            "Immich" = {
+              href = "http://10.66.0.10:2283";
+              description = "Photos & Videos";
+              icon = "immich.png";
+            };
+          }
+
+          {
+            "Jellyfin" = {
+              href = "http://10.66.0.10:8096";
+              description = "Media Server";
+              icon = "jellyfin.png";
+            };
+          }
+
+          {
+            "Nextcloud" = {
+              href = "http://10.66.0.10";
+              description = "Nextcloud Server";
+              icon = "nextcloud.png";
+            };
+          }
+        ];
+      }
+    ];
+
+    widgets = [
+      {
+        resources = {
+          cpu = true;
+          memory = true;
+          disk = "/";
+        };
+      }
+
+      {
+        datetime = {
+          format = "yyyy-MM-dd HH:mm";
+        };
+      }
+    ];
   };
-
-  bookmarks = [
-    {
-      "Network" = [
-        {
-          "TP-Link Router" = [
-            {
-              abbr = "TP";
-              href = "http://192.168.1.1";
-            }
-          ];
-        }
-      ];
-    }
-  ];
-
-  services = [
-    {
-      "Network" = [
-        {
-          "AdGuard Home" = {
-            href = "http://192.168.1.100:3002";
-            description = "DNS & Ad Blocking";
-            icon = "adguard-home.png";
-          };
-        }
-
-        {
-          "Router" = {
-            href = "http://192.168.1.1";
-            description = "TP-Link AX3000";
-            icon = "router.png";
-          };
-        }
-      ];
-    }
-
-    {
-      "Services" = [
-        {
-          "Immich" = {
-            href = "http://192.168.1.100:2283";
-            description = "Photos & Videos";
-            icon = "immich.png";
-          };
-        }
-
-        {
-          "Jellyfin" = {
-            href = "http://192.168.1.100:8096";
-            description = "Media Server";
-            icon = "jellyfin.png";
-          };
-        }
-
-        {
-          "PS2 OPL" = {
-            href = "smb://192.168.1.100";
-            description = "PS2 Game Share";
-            icon = "playstation.png";
-          };
-        }
-      ];
-    }
-  ];
-
-  widgets = [
-    {
-      resources = {
-        cpu = true;
-        memory = true;
-        disk = "/";
-      };
-    }
-
-    {
-      datetime = {
-        format = "yyyy-MM-dd HH:mm";
-      };
-    }
-  ];
-};
+}
