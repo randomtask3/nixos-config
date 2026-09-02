659e90b5cd9eebb82500495dae1f35bdc4e9aaba
randomtask3 <nbrooker@proton.me>
2026-09-02 19:46:44 -0400

    immich

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -1,18 +1,13 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 {
   services.immich = {
     enable = true;
     port = 2283;
     openFirewall = true;
     host = "10.66.0.10";
-    user = "nick";
+    #user = "nick" ;
     group = "users";
-
-    # PostgreSQL account
-    database.user = "immich";
     mediaLocation = "/srv/immich/data";
-
-    environment.DB_URL = lib.mkForce "postgresql://immich@/immich?host=/run/postgresql";
   };
   users.users.nick.extraGroups = [ "immich" ];
   environment.systemPackages = with pkgs; [
