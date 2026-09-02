de33a7d339ca3284f86e0157557a99cab7e35421
randomtask3 <nbrooker@proton.me>
2026-09-02 19:23:39 -0400

    immich changing user

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -5,8 +5,8 @@
     port = 2283;
     openFirewall = true;
     host = "10.66.0.10";
-    #user = "nick" ;
-    #group = "users";
+    user = "nick" ;
+    group = "users";
     mediaLocation = "/srv/immich/data";
   };
   environment.systemPackages = with pkgs; [
